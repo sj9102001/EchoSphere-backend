6ac7f3b02f38a65b6a1c74636a58c8e7bfdb398b
Sidarth Roy <sidarthroy@yahoo.com>
2024-06-10 14:57:00 +0530

    post commit

--- controllers/postController.js
+++ controllers/postController.js
@@ -79,72 +79,9 @@ exports.fetchPost=async(req,res)=>{
     }
 }
 
-// exports.fetchFeed = async (req, res) => {
-//     const authorId = req.user.id;
-//     try {
-//         const posts = await prisma.post.findMany({
-//             where: {
-//                 authorId: {
-//                     in: prisma.Friend.findMany({
-//                         where: {
-//                             user1Id: authorId
-//                         },
-//                         include: {
-//                             user2: true
-//                         }
-//                     }).then(friends => friends.map(friend => friend.user2Id))
-//                 }
-//             },
-//             orderBy: {
-//                 createdAt: 'desc'
-//             },
-//             include: {
-//                 author: {
-//                     select: {
-//                         name: true
-//                     }
-//                 }
-//             }
-//         });
-//         res.status(200).json({ message: "Feed Fetched", posts });
-//     } catch (error) {
-//         console.log(error);
-//         res.status(500).json({ message: "Internal Server Error"});
-//     }
-// }
-
-
 exports.fetchFeed=async(req,res)=>{
     const authorId=req.user.id
-    try{
-        const test = await prisma.user.findUnique({
-            where: {
-              id: authorId
-            },
-            select: {
-              friends: {
-                // select: {
-                //   friend: true
-                // },
-                where: {
-                  OR: [
-                    { user1Id: authorId },
-                    { user2Id: authorId }
-                  ]
-                }
-              },
-              posts: true
-            //    {
-            //     where: {
-            //       authorId: {
-            //         in: friends.flatMap(friend => friend.user1Id === authorId ? friend.user2Id : friend.user1Id)
-            //       }
-            //     }
-            //   }
-            }
-          });
-          
-
+    try{        
         const friendIds = await prisma.Friend.findMany({
             where:{
                 user1Id:authorId
@@ -168,68 +105,10 @@ exports.fetchFeed=async(req,res)=>{
                 }
             }
         })
-        res.status(200).json({message:"Feed Fetched",test})
+        res.status(200).json({message:"Feed Fetched",posts})
     }
     catch(error){
         console.log(error)
         res.status(500).json({message:"Internal Server Error"})
     }
 }
-
-
-
-// exports.fetchFeed = async (req, res) => {
-//     const authorId = req.user.id;
-
-//     try {
-//         // Step 1: Find all friends of the user
-//         const friends = await prisma.friend.findMany({
-//             where: {
-//                 OR: [
-//                     { user1Id: authorId },
-//                     { user2Id: authorId }
-//                 ]
-//             },
-//             select: {
-//                 user1Id: true,
-//                 user2Id: true
-//             }
-//         });
-
-//         // Extract friend IDs
-//         const friendIds = friends.flatMap(friend => 
-//             friend.user1Id === authorId ? friend.user2Id : friend.user1Id
-//         );
-
-//         // Step 2: Fetch posts from these friends
-//         const posts = await prisma.post.findMany({
-//             where: {
-//                 authorId: { in: friendIds }
-//             },
-//             orderBy: {
-//                 createdAt: 'desc'
-//             },
-//             include: {
-//                 author: true // Optional: to include author details in the response
-//             }
-//         });
-
-//         // Step 3: Return the posts
-//         res.status(200).json({ message: "Feed Fetched", posts });
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).json({ message: "Internal Server Error" });
-//     }
-// };
-
-// router.post('/create', verifyUser, postController.createPost);
-// router.delete('/delete', verifyUser, postController.deletePost);
-// router.put('/update', verifyUser, postController.updatePost);
-// router.get('/fetch', verifyUser, postController.fetchPost);
-// router.get('/feed', verifyUser, postController.fetchFeed);
-
-// add post
-// delete post
-//  update post
-//  fetch post of user
-//  fetch feed ( feed me basically, posts of all friends sorted by time )
